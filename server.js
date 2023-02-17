fdbace23d9991a6cdac2e00f5bd61e0653baa1e5
Elena Kochegarova <lelet@ngs.ru>
2023-02-17 21:42:43 +0700

    saving

--- server.js
+++ server.js
@@ -1,32 +1,56 @@
-// import Ticket from './src/Ticket';
-const { Ticket } = require('./src/Ticket');
-
 const http = require('http');
 const Koa = require('koa');
-const koaBody = require('koa-body');
+const { koaBody } = require('koa-body');
+const cors = require('@koa/cors');
 const { TicketFull } = require('./src/TicketFull');
 
 const app = new Koa();
+app.use(cors());
 
-const tickets = [
-  new TicketFull('Поменять краску в принтере', false, 'Принтер HP 1210'),
-  new TicketFull('Установить обновления ПО', true, 'каб. 201, каб. 301')
-];
+// const tickets = [
+//   new TicketFull('Поменять краску в принтере', false, 'Принтер HP 1210'),
+//   new TicketFull('Установить обновления ПО', true, 'каб. 201, каб. 301')
+// ];
 
-// app.use(koaBody({
-//   urlencoded: true,
-// }));
+let tickets = [];
 
-app.use((ctx, next) => {
-  // console.log('i am one');
-  console.log(ctx.headers);
-  // ctx.response.body = 'server response';
-  next();
-});
+app.use(koaBody({
+  urlencoded: true,
+  multipart: true,
+  json: true
+}));
 
-app.use(async (ctx) => {
-  // const { method } = ctx.request.querystring;
-  const method = 'createTicket';
+// устанавливаем разрешение на использование других методов при preflight-запросах
+// app.use(async (ctx, next) => {
+//   if (ctx.request.method !== 'OPTIONS') {
+//     next();
+//     return;
+//   }
+
+//   ctx.response.set('Access-Control-Allow-Origin', '*');
+//   ctx.response.set('Access-Control-Allow-Methods', 'DELETE, PUT, PATCH, GET, POST');
+//   ctx.response.status = 204;
+
+//   next();
+// });
+
+app.use(async (ctx, next) => {
+  console.log('ctx.request.body - ', ctx.request.body);
+  console.log('ctx.request.query.method - ', ctx.request.query.method);
+  let { method } = ctx.request.query;
+
+  // if (ctx.request.method === 'GET') {
+  //   ({ method } = ctx.request.query);
+  // }
+
+  // if (ctx.request.method === 'POST') {
+  //   ({ method } = ctx.request.body);
+  // }
+
+  // ctx.response.set('Access-Control-Allow-Origin', '*');
+  // ctx.response.set('Access-Control-Allow-Headers', ctx.request.get('Access-Control-Allow-Request-Headers'));
+
+  console.log('method запроса - ', ctx.request.method);
 
   switch (method) {
     case 'allTickets':
@@ -40,12 +64,18 @@ app.use(async (ctx) => {
       const ticket = new TicketFull(name, status, description);
       tickets.push(ticket);
       console.log(tickets);
+      ctx.response.body = ticket;
       return;
+    case 'changeTicket':
+
+    case 'deleteTicket':
 
     default:
       ctx.response.status = 404;
       return;
   }
+  // console.log('------------');
+  next();
 });
 
 const server = http.createServer(app.callback());
@@ -53,8 +83,10 @@ const port = 5050;
 
 server.listen(port, (err) => {
   if (err) {
+    console.log('------------');
     console.log('ошибка в listen', err);
     return;
   }
+  console.log('------------');
   console.log(`Server is listening to ${port}`);
 });
